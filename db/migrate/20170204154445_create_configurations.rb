48b82486fb6447f4c20e8cf5d687ff063f27dbd3
DainisL <dainis186@gmail.com>
2017-02-04 18:15:32 +0200

    update conf migration and added redcarpet to dep file

--- db/migrate/20170204154445_create_configurations.rb
+++ db/migrate/20170204154445_create_configurations.rb
@@ -3,6 +3,8 @@ ROM::SQL.migration do
     create_table :configurations do
       primary_key :id
       column :template, String, null: false
+      column :updated_at, Time, null: false
+      column :created_at, Time, null: false
     end
   end
 end
