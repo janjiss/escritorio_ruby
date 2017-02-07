9d3b846972993efa4cf4aa606b972aaac9522a70
Janis Miezitis <janjiss@gmail.com>
2017-02-07 08:23:32 -0500

    Removed comments from cusotm_render and template_render

--- lib/plugins/template_render.rb
+++ lib/plugins/template_render.rb
@@ -31,22 +31,6 @@ class Cuba
       APP.resolve("repos.configurations").get_config
     end
 
-    # @private Renders any type of template file supported by Tilt.
-    #
-    # @example
-    #
-    #   # Renders home, and is assumed to be HAML.
-    #   _render("home.haml")
-    #
-    #   # Renders with some local variables
-    #   _render("home.haml", site_name: "My Site")
-    #
-    #   # Renders with HAML options
-    #   _render("home.haml", {}, ugly: true, format: :html5)
-    #
-    #   # Renders in layout
-    #   _render("layout.haml") { _render("home.haml") }
-    #
     def _render(template, locals = {}, options = {}, &block)
       _cache.fetch(template) {
         Tilt.new(template, 1, options.merge(outvar: '@_output'))
