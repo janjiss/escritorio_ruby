7017c837fa1123da35bce6be437154ab0f2d4e9a
Janis Miezitis <janjiss@gmail.com>
2017-02-07 21:31:05 -0500

    Moved meta_title away from blog object, added meta info from post

--- lib/routes/public/posts.rb
+++ lib/routes/public/posts.rb
@@ -1,12 +1,15 @@
 class Escritorio::Routes::Public::Posts < Cuba
   define do
     config = APP.resolve('repos.configurations').get_config
+    blog   = config.blog
 
     on root do
       on get do
         posts = APP.resolve('repos.posts').all
         template_render "index", {
-          blog: config.blog,
+          blog: blog,
+          meta_title: blog.meta_title,
+          meta_description: blog.meta_description,
           posts: posts,
           pagination: ""
         }
@@ -18,6 +21,8 @@ class Escritorio::Routes::Public::Posts < Cuba
         post = APP.resolve('repos.posts').by_id(id)
         template_render "post", {
           current_url: "",
+          meta_title: post.meta_title,
+          meta_description: post.meta_description,
           post: post,
           blog: config.blog
         }
