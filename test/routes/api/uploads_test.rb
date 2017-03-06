71813cc9e496dac538c8698b1fc5dd22680c1ae7
Janis Miezitis <janjiss@gmail.com>
2017-03-05 21:10:24 -0500

    Fixed uploads route spec

--- test/routes/api/uploads_test.rb
+++ test/routes/api/uploads_test.rb
@@ -11,9 +11,9 @@ module RoutesTest
       id = "test"
       post "/api/uploads", { id: id, file: @file }
       assert_equal 200, last_response.status
-      response_body = { file: "/public/uploads/#{id}/test.png" }
+      response_body = { file: "/uploads/#{id}/test.png" }
       assert_equal response_body, Oj.load(last_response.body)
-      assert File.exists?(File.join(APP_DIR, response_body[:file]))
+      assert File.exists?(File.join(APP_DIR, File.join("public", response_body[:file])))
       FileUtils.rm_r(File.join(APP_DIR, "/public/uploads/#{id}"))
     end
   end
