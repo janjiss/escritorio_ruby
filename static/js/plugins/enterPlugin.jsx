714dd375eca0940d20905c68e2a0d2a689485317
Janis Miezitis <janjiss@gmail.com>
2017-03-09 23:21:13 -0500

    Fixed incorrect list splitting in the middle

--- static/js/plugins/enterPlugin.jsx
+++ static/js/plugins/enterPlugin.jsx
@@ -15,10 +15,32 @@ const setCurrentToDefaultBlock = (state) => {
     .apply()
 }
 
+const splitListAndInsertNewBlock = (state, document) => {
+  const transform = state.transform()
+  const { focusBlock } = state
+  const nextSibling = document.getNextSibling(focusBlock.key)
+
+  // This is super hacky. It check if the next item is list-item and 
+  // then splits it in to two, but the problem is that it still
+  // adds list tiem. That's why we are doing the deletion
+  if (nextSibling && nextSibling.type === 'list-item') {
+    return state.transform()
+      .unwrapBlock()
+      .splitBlock(2)
+      .unwrapBlock()
+      .insertBlock(DEFAULT_BLOCK.type)
+      .collapseToStartOfNextBlock()
+      .deleteForward(1)
+      .apply()
+  } else {
+    return setCurrentToDefaultBlock(state)
+  }
+}
+
 export default function enterPlugin() {
   return {
     onKeyDown: (e, data, state) => {
-      const { startOffset, focusBlock, isExpanded } = state
+      const { startOffset, focusBlock, isExpanded, document } = state
       const { type } = focusBlock
 
       // Ignore if key is not enter
@@ -28,7 +50,17 @@ export default function enterPlugin() {
 
       if (type.startsWith('header') || type == 'block-quote' ) {
         return insertDefaultBlock(state)
-      } else if (type == 'list-item' || type == 'code-block')  {
+      } else if (type == 'list-item')  {
+        e.preventDefault()
+
+        if (startOffset != 0) return
+        // Ignore if selection is not expanded
+        if (isExpanded) return
+        // We don't want to remove formatting if the blocks length is bigger than 0
+        if (focusBlock.length > 0) return
+        // Ignore we are not in 0 position
+        return splitListAndInsertNewBlock(state, document)
+      } else if (type == 'code-block') {
         if (startOffset != 0) return
         // Ignore if selection is not expanded
         if (isExpanded) return
